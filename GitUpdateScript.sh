b689f965f28241e4f195ae8caeded9333e69c175
0xNeutron <88306361+0xNeutron@users.noreply.github.com>
2022-06-17 15:18:04 +0530

    Changed status variables' scope to global, improved definitions of final restart functions, and added thought comments for restart mechanism. Need to recheck and refine Update_Windows_System_Drivers_Handle_Function and Change_DNS_Server_Update_Function

--- GitUpdateScript.sh
+++ GitUpdateScript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd C:\Users\abc\source\repos\Project_Automation\
+# cd C:\Users\abc\source\repos\Project_Automation\
 echo "[*] Checking repository status"
 git status &&
 echo "[*] Adding untracked files"
@@ -7,6 +7,6 @@ git add . &&
 echo "[*] Committing changes to local"
 git commit -am "Changed status variables' scope to global, improved definitions of final restart functions, and added thought comments for restart mechanism. Need to recheck and refine Update_Windows_System_Drivers_Handle_Function and Change_DNS_Server_Update_Function" &&
 echo "[*] Pushing changes to remote"
-git push origin main && 
+git push --force origin main && 
 echo "[*] Updating local from remote"
 git pull origin main
