be04ac4c8e20c9e60f56e93aa1f98e6333c3b92c
0xNeutron <88306361+0xNeutron@users.noreply.github.com>
2023-01-16 00:45:57 +0530

    Added utility function module

---
+++ Main/UtilFunc.psm1
@@ -0,0 +1,10 @@
+# ***************Base Information Sub-Section***************
+
+    # function Get_System_Information_Handle_Function {
+    #     # Get system information on the host
+    #     
+    # }
+
+# ***************END OF -> Base Information Sub-Section***************
+
+# will contain other utility functions
